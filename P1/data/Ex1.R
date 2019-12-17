88c5dbd00db22d916e6a20a833b09c8d531aae15
assmendes <56386978+assmendes@users.noreply.github.com>
2019-12-17 18:25:12 +0000

    ex1

--- P1/data/Ex1.R
+++ P1/data/Ex1.R
@@ -7,10 +7,12 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 
 # read in data
-crabs <- read.csv("crabs_preprocessed.csv")
+crabs <- data.frame(read.csv("crabs_preprocessed.csv"))
+
 
 ## Correlation matrix 
-Correl <- cor(crabs)
+Correl <- cor(crabs[3:5])
+
 
 col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
 heat_map_correlations <- corrplot(Correl, method = "ellipse", col = col(200),
@@ -21,7 +23,12 @@ heat_map_correlations <- corrplot(Correl, method = "ellipse", col = col(200),
                                   # hide correlation coefficient on the principal diagonal
                                   diag = FALSE)
 
+
 # Factorize color, and spine condition
+crabs$color = factor(crabs$color, levels =c(1,2,3,4,5))
+crabs$spine_condition = factor(crabs$spine_condition, levels = c(1,2,3))
+
+
 fit <- glm(satellites ~ ., data = crabs, family=poisson(link=log))
 summary(fit)
 
